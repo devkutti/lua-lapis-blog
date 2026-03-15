beca0ef775011a7df3ba2cb7c1d63d579303c35b
devkutti <deiwisson124@gmail.com>
2026-03-15 13:23:33 -0300

    add posts routes and templates

--- app.lua
+++ app.lua
@@ -4,8 +4,24 @@ local app = lapis.Application()
 app:enable("etlua")
 app.layout = require "views.layout"
 
+-- página inicial
 app:get("/", function(self)
   return { render = "index"}
+end)
+
+-- página com a lista de todos os posts
+app:get("/posts", function(self)
+  self.page_title = "Posts"
+  
+  return { render = "posts" }
+end)
+
+-- página de um post
+app:get("/posts/:id", function(self)
+  return {
+    render = "post",
+    id = self.params.id
+  }
 end)
 
 return app
